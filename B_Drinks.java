2281d0d59513c536309abe20cf48d71fe82afb5c
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-03-02 01:41:03 +0600

    200B - Drinks
    200B - Drinks problem's solution in java

---
+++ B_Drinks.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class B_Drinks {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int arr[] = new int[n];
+		
+		for(int i = 0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		double x = 0.00, ans = 0.00;
+		
+		for(int i = 0; i<n; i++) {
+			x = (double)arr[i] + x;
+		}
+		
+		x = x / 100.00;
+		
+		ans = (x / (double)n) * 100.00;
+		
+		System.out.println(ans);
+
+	}
+
+}
