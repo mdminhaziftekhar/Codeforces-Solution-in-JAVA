009874675917bc61959b7933d5c2f09488a7a015
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-05-16 08:08:31 +0600

    750A - New Year and Hurry

---
+++ 750A-New-Year-and-Hurry.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class New_Year_and_Hurry {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		int sum = 0, count = 0;
+		
+		if(k == 0) {
+			System.out.println(n);
+		}
+		else if(k == 240) {
+			System.out.println(0);
+		}
+		else {
+			for(int i = 1; i<=n; i++) {
+				sum = sum + (i * 5);
+				//System.out.println("Sum : "+sum);
+				if(sum+k > 240) {
+					break;
+				}
+				count++;
+			}
+			System.out.println(count);
+		}
+
+	}
+
+}
