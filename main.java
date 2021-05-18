26aeea1f0283886d47a6161a33ae9ad915edbe3e
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-05-18 23:43:21 +0600

    1409A - Yet Another Two Integers Problem

---
+++ main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		long t = scan.nextLong();
+		
+		while(t > 0) {
+			t--;
+			long ans = 0;
+			long a = scan.nextLong();
+			long b = scan.nextLong();
+			
+			if(a == b) {
+				ans = 0;
+			}
+			else if(b > a) {
+				a = b - a;
+				if(a > 10) {
+					ans = a / 10;
+					if(a%10!=0) {
+						ans = ans + 1;
+					}
+				}
+				else{
+					ans = 1;
+				}
+			}
+			else if(a > b) {
+				a = a - b;
+				if(a > 10) {
+					ans = a / 10;
+					if(a%10!=0) {
+						ans = ans + 1;
+					}
+					
+				}
+				else {
+					ans = 1;
+				}
+			}
+			
+			System.out.println(ans);
+		}
+
+	}
+
+}
