6deeca31f131ee509de290c550a1204eff26550c
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-05-20 09:44:48 +0600

    1154A - Restoring Three Numbers

---
+++ 1154A-Restoring_Three_Numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		ArrayList<Integer> v = new ArrayList<Integer>();
+		
+		for(int i = 0; i<4; i++) {
+			int a = scan.nextInt();
+			v.add(a);
+		}
+		
+		Collections.sort(v);
+		
+		int a = v.get(3) - v.get(2);
+		int b = v.get(3) - v.get(1);
+		int c = v.get(3) - v.get(0);
+		
+		System.out.println(a+" "+b+" "+c);
+		
+	}
+}
