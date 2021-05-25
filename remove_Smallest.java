779617355390d25a1a66180044a28d85dec0ddb4
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-05-25 23:59:31 +0600

    1399A - Remove Smallest

---
+++ remove_Smallest.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Remove_Smallest {
+
+	public static void remove_Smallest (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int t = scan.nextInt();
+
+		for(int i = 0; i<t; i++){
+            ArrayList<Integer> a = new ArrayList<>();
+            int size = scan.nextInt();
+
+            for(int j = 0; j<size; j++){
+                int b = scan.nextInt();
+                a.add(b);
+            }
+            Collections.sort(a);
+
+            int count = 1;
+
+            for(int j = 1; j<a.size(); j++){
+                int difference = a.get(j) - a.get(j-1);
+                if(difference > 1){
+                    count = 0;
+                    break;
+                }
+            }
+
+            if(count == 0){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+        }
+
+	}
+
+
+}
