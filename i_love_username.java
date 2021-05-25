262c31715c1819aa621b3183707ee9e36f9f25a1
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-05-26 00:54:30 +0600

    Update and rename I_love_username.java to i_love_username.java

--- i_love_username.java
+++ i_love_username.java
@@ -1,8 +1,7 @@
 import java.util.*;
-public class I_love_username {
+public class i_love_username {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner scan = new Scanner(System.in);
 		
